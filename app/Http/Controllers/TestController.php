062e17d6ad6294aef99294d05dc8c23697b53dbd
mehmethamza <hamzaininal@gmail.com>
2022-02-22 17:31:02 +0300

    addComment

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -13,6 +13,7 @@ class TestController extends Controller
     
     public function show($slug)
     {
+        // BUrda slug->tr gerek yok hatırla dökümanlarda modeli dile göre filtreleme vardı  a 
         $test = Test::where("slug->tr",$slug)->first(); 
         return $test;
        return view("test",compact("test"));
